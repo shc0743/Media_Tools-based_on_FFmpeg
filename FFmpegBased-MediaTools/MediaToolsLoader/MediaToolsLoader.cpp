7e3832959b238a3cce64d971459f63e538b8525f
shc0743 <shc0743@outlook.com>
2024-02-05 22:57:13 +0800

    搭了下架子；图标随便用脚画的，还请不要介意QAQ

---
+++ FFmpegBased-MediaTools/MediaToolsLoader/MediaToolsLoader.cpp
@@ -0,0 +1,43 @@
+﻿// : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#pragma comment(linker, "/subsystem:windows /entry:mainCRTStartup")
+
+#include "../../resource/tool.h"
+#include "ui.main.h"
+using namespace std;
+
+
+
+
+int main()
+{
+	CmdLineW cl(GetCommandLineW());
+
+	wstring type; cl.getopt(L"type", type);
+	
+
+
+	if (type == L"ui") {
+		return UiMain(cl);
+	}
+
+	if (type.empty() && cl.argc() < 2) {
+		return (int)Process.StartAndWait(L"\"" + GetProgramDirW() + L"\" --type=ui");
+	}
+
+	return ERROR_INVALID_PARAMETER;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ FFmpegBased-MediaTools/MediaToolsLoader/ui.main.cpp
@@ -0,0 +1,20 @@
+#include "ui.main.h"
+using namespace std;
+
+
+
+int UiMain(CmdLineW& cl) {
+	Sleep(5000);
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
